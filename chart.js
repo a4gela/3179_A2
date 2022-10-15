dd4f4d95069ebf71d58b8d675ec2efd859d3589d
a4gela <114207055+a4gela@users.noreply.github.com>
2022-10-16 00:45:04 +1100

    Add files via upload

--- chart.js
+++ chart.js
@@ -5,21 +5,21 @@ var victim_activity_radial = "charts/victim_activity_radial.json"
 var trend_line_bar = "charts/trend_line_bar.json"
 
 // Chart 1
-vegaEmbed("#incidents_map", incidents_map).then(function(result) {
+vegaEmbed("#incidents_map", incidents_map, {"actions":false}).then(function(result) {
 }).catch(console.error);
 
 // Chart 2
-vegaEmbed("#top_sharks_stacked", top_sharks_stacked).then(function(result) {
+vegaEmbed("#top_sharks_stacked", top_sharks_stacked, {"actions":false}).then(function(result) {
 }).catch(console.error);
 
 // Chart 3
-vegaEmbed("#trend_line_bar", trend_line_bar).then(function(result) {
+vegaEmbed("#trend_line_bar", trend_line_bar, {"actions":false}).then(function(result) {
 }).catch(console.error);
 
 // Chart 5
-vegaEmbed("#timing_bubble", timing_bubble).then(function(result) {
+vegaEmbed("#timing_bubble", timing_bubble, {"actions":false}).then(function(result) {
 }).catch(console.error);
 
 // Chart 6
-vegaEmbed("#victim_activity_radial", victim_activity_radial).then(function(result) {
+vegaEmbed("#victim_activity_radial", victim_activity_radial, {"actions":false}).then(function(result) {
 }).catch(console.error);
